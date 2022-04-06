b67e83244bc1487532212dc83e7c53aa1bf4a01a
Mroxny <Maciej.h.mrozek@gmail.com>
2022-04-06 20:50:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from ast import IsNot, Not
+from asyncio.windows_events import NULL
 from operator import ipow
 import tkinter as tk
 from tkinter import *
@@ -13,7 +15,11 @@ text_app_name = "Shutdown Manager"
 text_con_1 = "Select action:"
 text_execute = "Execute"
 
+
+
 root = tk.Tk()
+
+
 
 def shutdown():
 
@@ -32,21 +38,46 @@ def shutdown():
         action_type = "-r"
 
 
+    if len(entry.get()) <= 0:
+        value  = 0
+        entry.insert(END, value)
+
+    else:
+        try:
+            value = int(entry.get())
+
+        except:
+            print_error("Please insert numbers")
+            value = NULL
+
     
-    print("Shutdown now in ", time.get())
-    save_file()
+    
+    if value != NULL:
+        print("Shutdown now in", value, time.get())
+        save_file()
 
-    try:
-        ans = subprocess.check_output(["shutdown", str(action_type), "-t", str(10 * time_multiplier)])
+        try:
+            # ans = subprocess.check_output(["shutdown", str(action_type), "-t", str(value * time_multiplier)])
+            print(value)
+            print_error(clear=True)
 
-    except subprocess.CalledProcessError as e:
-        print(e.output)
-        print_error(msg="Error")
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+            print_error("Error")
 
 
-def print_error(msg):
-    errorLabel = Label(root, text= msg, font= ('Helvetica 10 bold'), fg="#ff0000")
-    errorLabel.place(relx=0.5, rely=0.65, anchor=CENTER)
+
+   
+
+
+def print_error(msg="",clear=False):
+
+    if clear == False:
+        errorLabel.config(text=msg)
+    else:
+        errorLabel.config(text="")
+
+
     
 
 
@@ -69,9 +100,13 @@ root.resizable(False, False)
 #Add a canvas widget
 canvas = Canvas(root)
 
-#Add a Label widget in the Canvas
-mainLabel = Label(canvas, text= text_con_1, font= ('Helvetica 17 bold'))
-mainLabel.pack(pady= 10, padx=40, side= tk.TOP, )
+#Add a main label in the Canvas
+label = Label(canvas, text= text_con_1, font= ('Helvetica 17 bold'))
+label.pack(pady= 10, padx=40, side= tk.TOP, )
+
+#Add a Error label in the Canvas
+errorLabel = Label(canvas, text="", font=('Helvetica 10 bold'), fg="#ff0000")
+errorLabel.pack(side=tk.TOP)
 
 #Create option menu 1
 action = StringVar(root)
@@ -79,12 +114,13 @@ action.set("shutdown") # default value
 w = OptionMenu(root, action, "shutdown", "restart",)
 w.place(relx=0.3, rely=0.5, anchor=CENTER)
 
-mainLabel = Label(root, text= "in", font= ('Helvetica 8 bold'))
-mainLabel.place(relx=0.45, rely=0.5, anchor=CENTER)
+label = Label(root, text= "in", font= ('Helvetica 8 bold'))
+label.place(relx=0.45, rely=0.5, anchor=CENTER)
 
 #Create an Input to read values
 entry= Entry(root, width= 5)
-entry.focus_set()
+
+entry.insert(END, '0')
 entry.place(relx=0.53, rely=0.5, anchor=CENTER)
 
 #Create option menu 2
